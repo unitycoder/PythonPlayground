058258d55bf56b46bc0a1041242f17c4fcad572d
mika <unitycoder@users.noreply.github.com>
2016-11-25 11:45:20 +0800

    Create call-function-with-dictionary-vars.py

---
+++ Functions/call-function-with-dictionary-vars.py
@@ -0,0 +1,11 @@
+# calls  function with vars/params coming from dictionary
+
+# sample functions
+def mytest(amount=0,other=10):
+    print "received=",amount,other
+
+# dictionary of vars
+myvars = {'amount': 10, 'other': 0}
+
+# ** means that the argument is a dictionary
+mytest(**myvars)
